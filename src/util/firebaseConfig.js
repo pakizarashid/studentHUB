a257cdc80ffa2b30a43ac42f1a37af9bc49b2214
Pakeeza Rasheed <67373357+pakizarashid@users.noreply.github.com>
2024-08-10 03:11:04 +0500

    Update firebaseConfig.js

--- src/util/firebaseConfig.js
+++ src/util/firebaseConfig.js
@@ -5,15 +5,15 @@ import "firebase/compat/firestore";
 import "firebase/compat/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjSPhRWPWGx_Qe6BLqAo2IQHfRYCPEom8",
-  authDomain: "studenthub-e3e5a.firebaseapp.com",
+  apiKey: "",
+  authDomain: "",
   databaseURL:
-    "https://studenthub-e3e5a-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "studenthub-e3e5a",
-  storageBucket: "studenthub-e3e5a.appspot.com",
-  messagingSenderId: "513428265090",
-  appId: "1:513428265090:web:1ef5e6929bcdf0877f3c9b",
-  measurementId: "G-F5QVET8MP1",
+    "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 if (!firebase.apps.length) {
